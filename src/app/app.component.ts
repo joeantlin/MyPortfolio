586320f39656a9203b093f6093229fe8caf7aee1
Joe Linares <bwajal@gmail.com>
2022-11-09 23:41:29 -0800

    added unity && page switch fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,25 @@
-import { Component } from '@angular/core';
-import { fade, fadeF, fadeB, fadeFbtn, fadeBbtn } from './Animations';
+import { ViewportScroller } from "@angular/common";
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { fade, fadeF, fadeB, fadeFbtn, fadeBbtn } from "./Animations";
 declare var $: any;
 // import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
- 
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
-  animations: [
-    fade,
-    fadeF,
-    fadeB,
-    fadeFbtn,
-    fadeBbtn
-  ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.sass"],
+  animations: [fade, fadeF, fadeB, fadeFbtn, fadeBbtn],
 })
 export class AppComponent {
   public show: boolean;
-  public title = 'public';
+  public title = "public";
   public currentYear: number = new Date().getFullYear();
 
-
-  constructor(
-    // private _route: ActivatedRoute,
-    // private _router: Router
-  ) {}
+  constructor(private scroller: ViewportScroller) {} // private _router: Router // private _route: ActivatedRoute,
 
   ngOnInit() {
-    this.show = true;
+    this.show = false;
 
     // window.addEventListener("load", function() {
     //     const loader = this.document.querySelector(".preloader");
@@ -35,66 +27,70 @@ export class AppComponent {
     // });
 
     $(document).ready(() => {
-      $(".next").click(function() {
-        var $target = $('.project.active').next('.project');
-        if ($target.length == 0)
-          $target = $('.project:first');
-        $('html, body').animate({
-          scrollTop: $target.offset().top
-        }, 10);
-      
-        $('.active').removeClass('active');
-        $target.addClass('active');
+      $(".next").click(function () {
+        var $target = $(".project.active").next(".project");
+        if ($target.length == 0) $target = $(".project:first");
+        $("html, body").animate(
+          {
+            scrollTop: $target.offset().top,
+          },
+          10
+        );
+
+        $(".active").removeClass("active");
+        $target.addClass("active");
       });
-      
     });
 
-    
+    var scroll =
+      window.requestAnimationFrame ||
+      function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+      };
 
-    var scroll = window.requestAnimationFrame || 
-    function(callback) {
-      window.setTimeout(callback, 1000/60)
-    };
-
-    var backgroundChange = document.querySelectorAll('.logo');
+    var backgroundChange = document.querySelectorAll(".logo");
 
     function changeB() {
-      backgroundChange.forEach(function(element){
+      backgroundChange.forEach(function (element) {
         if (isAnyPartOfElementInViewport(element)) {
           // document.body.style.backgroundColor = 'rgba(53, 53, 53, 1)'
-          document.body.style.backgroundColor = 'rgba(73, 73, 73, 1)'
+          document.body.style.backgroundColor = "rgba(73, 73, 73, 1)";
         } else {
           // document.body.style.backgroundColor = 'rgba(83, 83, 83, 1)'
-          document.body.style.backgroundColor = 'rgba(106, 108, 110, 1)'
+          document.body.style.backgroundColor = "rgba(106, 108, 110, 1)";
         }
       });
-      scroll(changeB)
+      scroll(changeB);
     }
     changeB();
-  
+
     function isAnyPartOfElementInViewport(el) {
       const rect = el.getBoundingClientRect();
-      const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-      const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
-      const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
-      const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
-      return (vertInView && horInView);
+      const windowHeight =
+        window.innerHeight || document.documentElement.clientHeight;
+      const windowWidth =
+        window.innerWidth || document.documentElement.clientWidth;
+      const vertInView =
+        rect.top <= windowHeight && rect.top + rect.height >= 0;
+      const horInView = rect.left <= windowWidth && rect.left + rect.width >= 0;
+      return vertInView && horInView;
     }
   }
-  
-  
-  
+
   showFront(): void {
-    var fullpage = document.getElementById("full");
-    var frontpage = document.getElementById("front");
-    if (this.show == true){
-      this.show = false;
-    //   fullpage.style.color = "white";
-    //   frontpage.style.color = "rgba(41, 171, 226, 1)";
-    //   setTimeout(ifOne, 1500)
-    } else {
-      this.show = true;
-  
-    }
+    this.show = !this.show;
+	let anchor = "top" + (this.show ? "_front" : "_back");
+	console.log(anchor);
+	
+	
+	setTimeout(() => {
+		// this.scroller.scrollToAnchor(anchor);
+		document.getElementById(anchor).scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest"
+		  });
+	}, 1000)
+	
   }
 }
